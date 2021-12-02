68e57e0c81945b772c5af04fdae0c19406ded85e
Fiqrian <fiqrian.faturahman@gmail.com>
2021-12-02 20:12:53 +0700

    tugas 17 javascript

---
+++ tugas17.js
@@ -0,0 +1,30 @@
+const Tugas17 = () => {
+  let x = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+  let y =
+    "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual";
+  console.log(x.toLowerCase());
+  console.log(x.toUpperCase());
+  console.log(y.toLowerCase());
+  console.log(y.toUpperCase());
+};
+Tugas17();
+console.log("***********************");
+const ASCII = () => {
+  let x =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
+  console.log(
+    x.charCodeAt(0),
+    "-",
+    x.charCodeAt(1),
+    "-",
+    x.charCodeAt(2),
+    "-",
+    x.charCodeAt(3),
+    "-",
+    x.charCodeAt(4),
+    "-",
+    x.charCodeAt(5)
+  );
+};
+
+ASCII();
